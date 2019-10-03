17fd962ff04b4f48c9f6a6098cec85edfd7ccb76
Skemc <eric.mico6@gmail.com>
2019-10-04 00:01:44 +0200

    feat(backend): delete article -User can delete article -test -Add data to the model [Finishes: #168939254]

--- Server/Controllers/article.controller.js
+++ Server/Controllers/article.controller.js
@@ -3,11 +3,12 @@ import articles from '../Models/article.model';
 import validateArticles from '../Helper/article.validations';
  
 class ArticleController{
+  
   static createArticle(req,res){
     const { error } = validateArticles.validateArticle(req.body);
     if (error){
       return res.status(400).send({ status: 400, error: error.details[0].message });
-  }
+    }
     const loggedUser = req.user.email;
     const isUserExist = users.find(user=>user.email==loggedUser);
     const isArticleExist = articles.find(article=>article.title===req.body.title && article.article===req.body.article);
@@ -39,6 +40,7 @@ class ArticleController{
       data
     });
   }
+
   static editArticle(req, res) {
     const { title, article } = req.body;
     const { articleId } = req.params;
@@ -76,6 +78,30 @@ class ArticleController{
       data
     });
   }
+
+  static deleteArticle(req, res) {
+    const { articleId } = req.params;
+    const logged = req.user.email;
+    const isArticleExist = articles.find(art => art.id === parseInt(articleId));
+    if (!isArticleExist) {
+      return res.status(404).send({
+        status: 404,
+        error: 'Article Not found'
+      });
+    } 
+    if (isArticleExist.authorId !== logged) {
+      return res.status(403).send({
+        status: 403,
+        error: 'Article Not Yours'
+      });
+    }
+    articles.splice(articles.indexOf(isArticleExist, 1));
+    return res.status(200).send({
+      status: 200,
+      message: "Deleted successfully"
+    });
+  }
+
 }
 
 export default ArticleController;
--- Server/Routes/articler.routes.js
+++ Server/Routes/articler.routes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post('/articles', verifyUser, articleController.createArticle);
 router.patch('/articles/:articleId', verifyUser, articleController.editArticle);
+router.delete('/articles/:articleId', verifyUser, articleController.deleteArticle);
 
 export default router;
