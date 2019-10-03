3851102414949c3de941f9abb992d7c47fa5e2b7
Skemc <eric.mico6@gmail.com>
2019-10-04 01:11:47 +0200

    feat(backend): view articles -User can view articles -test -Add data to the model [Finishes: #168941415]

--- Server/Routes/articler.routes.js
+++ Server/Routes/articler.routes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/articles', verifyUser, articleController.createArticle);
 router.patch('/articles/:articleId', verifyUser, articleController.editArticle);
 router.delete('/articles/:articleId', verifyUser, articleController.deleteArticle);
+router.get('/articles', verifyUser, articleController.viewArticles);
 
 export default router;
